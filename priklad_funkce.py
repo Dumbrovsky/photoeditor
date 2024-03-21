68bb59a936f2d369752ac9f5a64e9e67fd74d395
Dumbrovsky <148480956+Dumbrovsky@users.noreply.github.com>
2024-03-21 18:41:31 +0100

    nové funkce def

--- priklad_funkce.py
+++ priklad_funkce.py
@@ -1,10 +1,32 @@
-def soucet(a, b):
-    return a+b
+from PIL import Image
 
-def rozdil(a, b):
-    return a-b
+def grayscale_filter(image):
+    width, height = image.size
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            avg = int((r + g + b) / 3)
+            image.putpixel((x, y), (avg, avg, avg))
+    return image
 
+def invert_filter(image):
+    return image.convert('RGB').point(lambda p: 255 - p)
 
-### menu
+def red_blue_filter(image):
+    width, height = image.size
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            image.putpixel((x, y), (r, b, r))
+    return image
 
-### cyklus / volba / aktivace funkce
+def apply_filter(image, choice):
+    if choice == 1:
+        return grayscale_filter(image)
+    elif choice == 2:
+        return invert_filter(image)
+    elif choice == 3:
+        return red_blue_filter(image)
+    else:
+        print("Invalid choice")
+        return image
